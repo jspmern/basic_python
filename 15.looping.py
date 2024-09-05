cd3f725fb4c0378d335fa56fbad9c103f23efd40
jspmern <134581836+jspmern@users.noreply.github.com>
2024-09-05 19:23:30 +0530

    loop is done

---
+++ 15.looping.py
@@ -0,0 +1,22 @@
+# i=1
+# while (i<5):
+#     print('hello')
+#     i=i+1
+
+# i=0
+# fruits=['apple','mango','orange','grapes']
+# x=len(fruits)
+# while i < (x) :
+#     if(fruits[i]=="mango"):
+#         break
+#     print(fruits[i])
+#     i=i+1
+
+for i in range(6):
+    print('hello')
+
+for i in range(len(['a','b','c'])):
+    print(i)
+
+p=range(5)
+print(type(p))

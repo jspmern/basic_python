793eb046247bd564c92ff76663f137da1ff4c495
jspmern <134581836+jspmern@users.noreply.github.com>
2024-09-05 19:07:25 +0530

    conditonal statement is done

---
+++ condition.py
@@ -0,0 +1,28 @@
+# age=18
+# if age>11:
+#     print('eligible for vote')
+# age=1
+# if age>10:
+#     print('eligible for vote')
+# else:
+#     print('not eligible for vote')
+# 
+# age=12
+# if age>12:
+#     print('a')
+# else:
+#     print('c')
+# age=120
+# if age>12:
+#     print('a')
+# else:
+#     print('c')
+
+age=12
+if age>15:
+    print('a')
+elif age>5 and age<100:
+    print('b')
+else:
+    print('c')
+ 

867fd70dbbb906f5d43967634bfa756c8b135d9a
jspmern <134581836+jspmern@users.noreply.github.com>
2024-09-06 18:22:58 +0530

    class and object is done

---
+++ classandobject.py
@@ -0,0 +1,15 @@
+#creating class
+# class Student:
+#     name="utsav kumar jha"
+
+# #creating object(instance)
+# s1=Student()
+# print(s1)
+# print(s1.name)
+
+
+class Student:
+    name="utsav kumar jha"
+
+s1=Student()
+print(s1.name)
